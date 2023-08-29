3ff408258376aa60a3f94a6cf4fad548aa0fbeb2
Sethumadhavan Kochukrishnan potti <sethumadhavankochukrishnanpotti@Sethumadhavans-MacBook-Pro.local>
2023-08-29 16:50:14 +1000

    uploadthing set up

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
  
-export default function Page() {
-  return <SignIn />;
+export default function Page({searcParams}:any) {
+  const {redirectUrl}= searcParams
+  return <SignIn redirectUrl={redirectUrl || "/"} />;
 }
